c7c750b127c99ef8b9b11d5666858fec03d4734b
limyuanlinkevin <kevin_raffles@hotmail.com>
2013-07-09 16:33:01 +0000

    still without buf2str

--- web.js
+++ web.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index2.html'))
-;
+  response.send(fs.readFileSync('index2.html', encoding));
 });
 
 var port = process.env.PORT || 5000;
