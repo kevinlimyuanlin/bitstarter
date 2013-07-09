224559c029e9596aadd7454b20c9ba66c3585b89
limyuanlinkevin <kevin_raffles@hotmail.com>
2013-07-09 16:13:57 +0000

    try harder

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("hello world");
+  response.send(buf.toString(fs.readFileSync('index2.html')));
 });
 
 var port = process.env.PORT || 5000;
