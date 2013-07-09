eaa7fd89aa77ab45aad137afdfd9cfa62ace7cf7
limyuanlinkevin <kevin_raffles@hotmail.com>
2013-07-09 16:10:00 +0000

    poor typing

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("hello world"1);
+  response.send("hello world");
 });
 
 var port = process.env.PORT || 5000;
